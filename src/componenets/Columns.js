49099fc0e665a4322bf9ce7ddb4bb93f9bebee5c
muskanbatra-dev <95659002+muskanbatra-dev@users.noreply.github.com>
2022-03-22 05:38:57 -0700

    created column.js

---
+++ src/componenets/Columns.js
@@ -0,0 +1,23 @@
+export const COLUMNS =[
+    {
+        Header:'Id',
+        accessor:'id'
+    },
+    {
+        Header:'first Name',
+        accessor:'first_name'
+    },
+    {
+        Header:'last Name',
+        accessor:'last_name'
+    },
+    {
+        Header:'email',
+        accessor:'email'
+    },
+    {
+        Header:'gender',
+        accessor:'gender'
+    },
+    
+]
